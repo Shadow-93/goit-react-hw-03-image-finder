249c4e153acd27dbd3846a57115bc55dd98c987d
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-09-17 20:29:45 +0300

    Updates

---
+++ precache-manifest.e4d0d1b88615b64039ffdbab76778197.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8e9b9465722cfcfd39c89315d11a7a",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "52b1905e990c4df2ecc1",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "2ec3aaa6905b5fe34741",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.de21201d.chunk.css"
+  },
+  {
+    "revision": "52b1905e990c4df2ecc1",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d9e4ec0f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.d9e4ec0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ec3aaa6905b5fe34741",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.f99d6a56.chunk.js"
+  },
+  {
+    "revision": "00ed64b4697a879a9e95",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.4e03020b.js"
+  }
+]);
