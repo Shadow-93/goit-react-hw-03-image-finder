99326f60c768a58a9120926fe2573f51008c12d1
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-12-20 17:11:19 +0200

    some fixes

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -2,7 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ImageGalleryItem = ({ tags, webformatURL, imgId, onClickItem }) => {
-  console.log(imgId);
   return (
     <li className="ImageGalleryItem">
       <img
