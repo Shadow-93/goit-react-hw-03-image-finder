6b020ccec6765cfcee45640e8b19234ee8079559
Andrey Hryhoriev <andreygrigoryev@ukr.net>
2020-12-24 21:03:58 +0200

    Updates

--- precache-manifest.906c0a4ddec0ce329e4bee649c394d04.js
+++ precache-manifest.906c0a4ddec0ce329e4bee649c394d04.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "781df5ceef7e117e712ba127d8b04351",
+    "revision": "7f7ce1d978ced8035ba62dfdb24e23e7",
     "url": "/goit-react-hw-03-image-finder/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/css/2.f87e9f44.chunk.css"
   },
   {
-    "revision": "9e2bf086046231119bcd",
+    "revision": "d2c794fb9c22c6e824f2",
     "url": "/goit-react-hw-03-image-finder/static/css/main.de21201d.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-03-image-finder/static/js/2.f9fdc43e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9e2bf086046231119bcd",
-    "url": "/goit-react-hw-03-image-finder/static/js/main.725da8a4.chunk.js"
+    "revision": "d2c794fb9c22c6e824f2",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.2d7c498f.chunk.js"
   },
   {
     "revision": "00ed64b4697a879a9e95",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-03-image-finder/precache-manifest.b98c36e4c55a6236c27ec9b8ead5259e.js"
+  "/goit-react-hw-03-image-finder/precache-manifest.906c0a4ddec0ce329e4bee649c394d04.js"
 );
 
 self.addEventListener('message', (event) => {
